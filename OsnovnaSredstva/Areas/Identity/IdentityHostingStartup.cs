30109135096caa1ae32f2d51cf945403a6f14cc6
Robert Bakota <robakota@vsite.hr>
2021-02-01 22:35:37 +0100

    scaffolded Login, Logout and Register pages into project

---
+++ OsnovnaSredstva/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OsnovnaSredstva.Data;
+
+[assembly: HostingStartup(typeof(OsnovnaSredstva.Areas.Identity.IdentityHostingStartup))]
+namespace OsnovnaSredstva.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
